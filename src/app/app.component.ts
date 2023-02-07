4b031457bc03841e397195e4fe00efface33c68b
Marius Bolik <marius@eyloo.com>
2023-02-07 14:59:05 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Stripe } from '@capacitor-community/stripe';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor() {
+    Stripe.initialize({
+      publishableKey: 'pk_test_51KFDksKRG9PRcrzzFTufGURQI0o6bpWmfKE2j38nn0bTHIrLDq0nWyD4j6Y6weguOBJL4y5qrmPJCWzGKioy4uOX00icJMyhaP',
+
+      /**
+       * Danger: This is production environment using production key.
+       * For testing ApplePay and GooglePay, but If it fails, payment will occur.
+       */
+      // publishableKey: 'pk_live_51KFDksKRG9PRcrzztDRkrFSon0jOxWuQ77zd2URAyn3sy4Dn1EST360KnM6ElTlAerKOBvi27J4SPlKd2rG4SNAZ00n0f3mEbg',
+    });
+  }
+}
---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,270 @@
+import {Component, OnInit} from '@angular/core';
+import {ApplePayEventsEnum, GooglePayEventsEnum, PaymentFlowEventsEnum, PaymentSheetEventsEnum, Stripe} from '@capacitor-community/stripe';
+
+import {environment} from '../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {first} from 'rxjs/operators';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit {
+  processSheet: 'willReady' | 'Ready' = 'willReady';
+  processFlow: 'willReady' | 'Ready' | 'canConfirm' = 'willReady';
+  processApplePay: 'willReady' | 'Ready' = 'willReady';
+  processGooglePay: 'willReady' | 'Ready' = 'willReady';
+  isApplePayAvailable = false;
+  isGooglePayAvailable = false;
+
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  async ngOnInit() {
+    Stripe.addListener(PaymentSheetEventsEnum.Loaded, () => {
+      this.processSheet = 'Ready';
+      console.log('PaymentSheetEventsEnum.Loaded');
+    });
+
+    Stripe.addListener(PaymentSheetEventsEnum.FailedToLoad, () => {
+      console.log('PaymentSheetEventsEnum.FailedToLoad');
+    });
+
+    Stripe.addListener(PaymentSheetEventsEnum.Completed, () => {
+      this.processSheet = 'willReady';
+      console.log('PaymentSheetEventsEnum.Completed');
+    });
+
+    Stripe.addListener(PaymentSheetEventsEnum.Canceled, () => {
+      this.processSheet = 'willReady';
+      console.log('PaymentSheetEventsEnum.Canceled');
+    });
+
+    Stripe.addListener(PaymentSheetEventsEnum.Failed, () => {
+      this.processSheet = 'willReady';
+      console.log('PaymentSheetEventsEnum.Failed');
+    });
+
+    /** ------------------------------------------------------------------- **/
+
+    Stripe.addListener(PaymentFlowEventsEnum.Loaded, () => {
+      this.processFlow = 'Ready';
+      console.log('PaymentFlowEventsEnum.Loaded');
+    });
+
+    Stripe.addListener(PaymentFlowEventsEnum.FailedToLoad, () => {
+      console.log('PaymentFlowEventsEnum.FailedToLoad');
+    });
+
+    Stripe.addListener(PaymentFlowEventsEnum.Completed, () => {
+      this.processFlow = 'willReady';
+      console.log('PaymentFlowEventsEnum.Completed');
+    });
+
+    Stripe.addListener(PaymentFlowEventsEnum.Canceled, () => {
+      this.processFlow = 'willReady';
+      console.log('PaymentFlowEventsEnum.Canceled');
+    });
+
+    Stripe.addListener(PaymentFlowEventsEnum.Failed, () => {
+      this.processFlow = 'willReady';
+      console.log('PaymentFlowEventsEnum.Failed');
+    });
+
+    Stripe.addListener(PaymentFlowEventsEnum.Created, (info) => {
+      console.log(info);
+      this.processFlow = 'canConfirm';
+    });
+
+    /** ------------------------------------------------------------------- **/
+
+    Stripe.addListener(ApplePayEventsEnum.Loaded, () => {
+      this.processApplePay = 'Ready';
+      console.log('ApplePayEventsEnum.Loaded');
+    });
+
+    Stripe.addListener(ApplePayEventsEnum.FailedToLoad, () => {
+      console.log('ApplePayEventsEnum.FailedToLoad');
+    });
+
+    Stripe.addListener(ApplePayEventsEnum.Completed, () => {
+      this.processApplePay = 'willReady';
+      console.log('ApplePayEventsEnum.Completed');
+    });
+
+    Stripe.addListener(ApplePayEventsEnum.Canceled, () => {
+      this.processApplePay = 'willReady';
+      console.log('ApplePayEventsEnum.Canceled');
+    });
+
+    Stripe.addListener(ApplePayEventsEnum.Failed, () => {
+      this.processApplePay = 'willReady';
+      console.log('ApplePayEventsEnum.Failed');
+    });
+
+    Stripe.addListener(ApplePayEventsEnum.DidCreatePaymentMethod, (data) => {
+      console.log(['ApplePayEventsEnum.DidCreatePaymentMethod', data.hasOwnProperty('contact')]);
+    });
+
+    Stripe.addListener(ApplePayEventsEnum.DidSelectShippingContact, (data) => {
+      console.log(['ApplePayEventsEnum.DidSelectShippingContact', data.hasOwnProperty('contact')]);
+    });
+
+    /** ------------------------------------------------------------------- **/
+
+    Stripe.addListener(GooglePayEventsEnum.Loaded, () => {
+      this.processGooglePay = 'Ready';
+      console.log('GooglePayEventsEnum.Loaded');
+    });
+
+    Stripe.addListener(GooglePayEventsEnum.FailedToLoad, () => {
+      console.log('GooglePayEventsEnum.FailedToLoad');
+    });
+
+    Stripe.addListener(GooglePayEventsEnum.Completed, () => {
+      this.processGooglePay = 'willReady';
+      console.log('GooglePayEventsEnum.Completed');
+    });
+
+    Stripe.addListener(GooglePayEventsEnum.Canceled, () => {
+      this.processGooglePay = 'willReady';
+      console.log('GooglePayEventsEnum.Canceled');
+    });
+
+    Stripe.addListener(GooglePayEventsEnum.Failed, () => {
+      this.processGooglePay = 'willReady';
+      console.log('GooglePayEventsEnum.Failed');
+    });
+
+    Stripe.isApplePayAvailable().then(() => this.isApplePayAvailable = true).catch(() => undefined);
+    Stripe.isGooglePayAvailable().then(() => this.isGooglePayAvailable = true).catch(() => undefined);
+  }
+
+  async createPaymentSheet(withCustomer = true) {
+    if (withCustomer) {
+      const { paymentIntent, ephemeralKey, customer } = await this.http.post<{
+        paymentIntent: string;
+        ephemeralKey: string;
+        customer: string;
+      }>(environment.api + 'intent', {}).pipe(first()).toPromise(Promise);
+
+      await Stripe.createPaymentSheet({
+        paymentIntentClientSecret: paymentIntent,
+        customerEphemeralKeySecret: ephemeralKey,
+        customerId: customer,
+        merchantDisplayName: 'rdlabo',
+      });
+    } else {
+      const { paymentIntent } = await this.http.post<{
+        paymentIntent: string;
+      }>(environment.api + 'intent/without-customer', {}).pipe(first()).toPromise(Promise);
+
+      await Stripe.createPaymentSheet({
+        paymentIntentClientSecret: paymentIntent,
+        merchantDisplayName: 'rdlabo',
+      });
+    }
+  }
+
+  async createPaymentSheetWithSetupIntent() {
+    const { setupIntent, ephemeralKey, customer } = await this.http.post<{
+      setupIntent: string;
+      ephemeralKey: string;
+      customer: string;
+    }>(environment.api + 'intent/setup', {}).pipe(first()).toPromise(Promise);
+
+    await Stripe.createPaymentSheet({
+      setupIntentClientSecret: setupIntent,
+      customerEphemeralKeySecret: ephemeralKey,
+      customerId: customer,
+      merchantDisplayName: 'rdlabo',
+      enableGooglePay: true,
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      GooglePayIsTesting: true,
+    });
+  }
+
+  presentPaymentSheet() {
+    return Stripe.presentPaymentSheet();
+  }
+
+  async createPaymentFlow(withCustomer = true) {
+    if (withCustomer) {
+      const { paymentIntent, ephemeralKey, customer } = await this.http.post<{
+        paymentIntent: string;
+        ephemeralKey: string;
+        customer: string;
+      }>(environment.api + 'intent', {}).pipe(first()).toPromise(Promise);
+
+      await Stripe.createPaymentFlow({
+        paymentIntentClientSecret: paymentIntent,
+        customerEphemeralKeySecret: ephemeralKey,
+        customerId: customer,
+        merchantDisplayName: 'rdlabo',
+      });
+    } else {
+      const { paymentIntent } = await this.http.post<{
+        paymentIntent: string;
+      }>(environment.api + 'intent/without-customer', {}).pipe(first()).toPromise(Promise);
+
+      await Stripe.createPaymentFlow({
+        paymentIntentClientSecret: paymentIntent,
+        merchantDisplayName: 'rdlabo',
+      });
+    }
+  }
+
+  presentPaymentFlow() {
+    return Stripe.presentPaymentFlow();
+  }
+
+  confirmPaymentFlow() {
+    return Stripe.confirmPaymentFlow();
+  }
+
+  async createApplePay() {
+    const { paymentIntent } = await this.http.post<{
+      paymentIntent: string;
+    }>(environment.api + 'intent', {}).pipe(first()).toPromise(Promise);
+
+    await Stripe.createApplePay({
+      paymentIntentClientSecret: paymentIntent,
+      paymentSummaryItems: [{
+        label: 'Product Name',
+        amount: 1099.00
+      }],
+      merchantIdentifier: 'merchant.com.getcapacitor.stripe',
+      requiredShippingContactFields: ['postalAddress', 'phoneNumber', 'emailAddress', 'name'],
+      countryCode: 'US',
+      currency: 'USD',
+    });
+  }
+
+  presentApplePay() {
+    return Stripe.presentApplePay();
+  }
+
+  async createGooglePay() {
+    const { paymentIntent } = await this.http.post<{
+      paymentIntent: string;
+    }>(environment.api + 'intent', {}).pipe(first()).toPromise(Promise);
+
+    await Stripe.createGooglePay({
+      paymentIntentClientSecret: paymentIntent,
+      paymentSummaryItems: [{
+        label: 'Product Name',
+        amount: 1099.00
+      }],
+      merchantIdentifier: 'merchant.com.getcapacitor.stripe',
+      countryCode: 'US',
+      currency: 'USD',
+    });
+  }
+
+  async presentGooglePay() {
+    return Stripe.presentGooglePay();
+  }
+}
